e5f891b2af038b092214065a346b361d6225051c
nagyl1999 <45666481+nagyl1999@users.noreply.github.com>
2025-10-27 22:53:06 +0100

    Pylint errors fixed

--- muse2csv/converter.py
+++ muse2csv/converter.py
@@ -96,7 +96,7 @@ def process_waveforms(waveform: Dict[str, Any]) -> Dict[str, np.ndarray]:
 
         scale = UNIT_SCALE_MAP.get(lead["LeadAmplitudeUnits"].strip().upper())
         if scale is None:
-            raise ValueError(f"Unknown amplitude unit: {lead["LeadAmplitudeUnits"]}")
+            raise ValueError(f"Unknown amplitude unit: {lead['LeadAmplitudeUnits']}")
 
         lead_waveforms[lead_id] = samples * float(lead["LeadAmplitudeUnitsPerBit"]) * scale # convert to mV
         logger.debug("Processed lead %s: %s samples.", lead_id, len(samples))
